17b78f3ed27ae297a6ab668769b9e7bef6c35005
Manion-sebastian <Sjmanion123@gmail.com>
2022-09-02 10:41:54 -0700

    init ejs layouts

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express')
 const app = express()
+const ejsLayouts = require('express-ejs-layouts')
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs') 
+app.use(ejsLayouts)
