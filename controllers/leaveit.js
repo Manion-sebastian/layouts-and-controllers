3f12683e37f9f887cee17bbdef47a3e6f0563796
Manion-sebastian <Sjmanion123@gmail.com>
2022-09-02 11:51:49 -0700

    functional

--- controllers/leaveit.js
+++ controllers/leaveit.js
@@ -2,11 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/movies', (req, res) => {
-    res.render('movies', {movies: ['The Godfather', 'Marvel']})
+    res.render('leaveit/movies', {movies: ['The Godfather', 'Marvel']})
 })
 
 router.get('/products', (req, res) => {
-    res.render('products', {products: ['swirl mop']})
+    res.render('leaveit/products', {products: ['swirl mop']})
 })
 
 module.exports = router
--- controllers/loveit.js
+++ controllers/loveit.js
@@ -2,11 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/animals', (req, res) => {
-    res.render('animals', {animals: ['sand crab', 'corny joke dog']})
+    res.render('loveit/animals', {animals: ['sand crab', 'corny joke dog']})
   })
 
 router.get('/food', (req, res) => {
-    res.render('food', {food: ['Tiramisu', 'Tteokbokki']})
+    res.render('loveit/food', {food: ['Tiramisu', 'Tteokbokki']})
 })
 
 module.exports = router
